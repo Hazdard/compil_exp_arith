0f8f615e5ecf3b4f803b5f5a7965f7b2307dae9e
hazdard <sacha.ben-arous@ens-paris-saclay.fr>
2022-10-22 22:02:52 -0500

    Peut maintenant prendre un fichier en entree

--- main.ml
+++ main.ml
@@ -3,9 +3,9 @@ open Lexer
 open Parser
 
 let _ =
-  let lexbuf = Lexing.from_channel stdin in
+  let lexbuf = Lexing.from_channel (open_in (Sys.argv.(1))) in  (* Mettre stdin pour lire directement le texte ecrit dans la console *)
   let ast = Parser.parse Lexer.token lexbuf in
   if not (Asyntax.bien_typee ast) then failwith "Erreur de typage"
   else Asyntax.afficher_sexp ast
 
-(* doit encore soccuper de la conversion flottants <-> entiers ; et de faire respecter le typage *)
+ 
